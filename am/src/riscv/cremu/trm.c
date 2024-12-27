9efff269e90abd05e271ab078f52c63fcedef9eb
charain <charain_li@outlook.com>
2024-12-27 15:21:36 +0800

    为cremu添加输出支持

--- am/src/riscv/cremu/trm.c
+++ am/src/riscv/cremu/trm.c
@@ -1,17 +1,19 @@
 #include <am.h>
 #include <klib-macros.h>
+#include "../riscv.h"
 
 extern char _heap_start;
 int main(const char *args);
 
 extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
+#define PMEM_SIZE (8 * 1024 * 1024)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
 void putch(char ch) {
+  outb(0x10000000, ch);
 }
 
 void halt(int code) {
